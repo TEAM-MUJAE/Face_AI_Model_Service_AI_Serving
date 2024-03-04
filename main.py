47e4b577c7915266f7212ee8dd130b96ed8d7c79
costudying <bnj021212@gmail.com>
2024-03-04 14:35:57 +0900

    20240304 fastapi router 연결

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 import comp_other as comp_other_router
 import comp_people as comp_people_router
+import comp_celeb as comp_celeb_router
+import os
+
+# 텐서플로우 버전 확인
+# import tensorflow as tf
+# print(tf.__version__)
+
+# oneDNN CPU performance optimizations
+# os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 app = FastAPI()
 
 app.include_router(comp_other_router.router)
 app.include_router(comp_people_router.router)
+app.include_router(comp_celeb_router.router)
