f5b5c7e017b95617bb6da8f87da9b50c38c74ddc
costudying <bnj021212@gmail.com>
2024-03-04 14:59:39 +0900

    20240304 HTTP 요청 시 파일 업로드 오류 수정

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 import comp_other as comp_other_router
 import comp_people as comp_people_router
 import comp_celeb as comp_celeb_router
-import os
+# import os
 
 # 텐서플로우 버전 확인
 # import tensorflow as tf
